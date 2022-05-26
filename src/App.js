79ca7a233b4b988ad1de1d2d2648f4799cad1858
diglean <diegoleandro2002@gmail.com>
2022-05-26 11:30:32 -0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect, useMemo} from 'react';
 
 function App() {
 
@@ -23,8 +23,7 @@ function App() {
     setInput('');
   }
 
-
-
+  const totalTarefas = useMemo(()=> tarefas.length, [tarefas]);
 
   return (
     <div>
@@ -33,6 +32,8 @@ function App() {
           <li key={tarefa}>{tarefa}</li>
         ))}
       </ul>
+      <br/>
+      <strong>Você tem: {totalTarefas} tarefas</strong>
       <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
           <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
